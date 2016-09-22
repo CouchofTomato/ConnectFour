73b66d368f215316624d83e00382f4f0c07fbab2
Austin P Mason <austinpmason@gmail.com>
2016-09-22 15:59:48 +0100

    starting over

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,9 +1,5 @@
 module ConnectFour
   class Game
 
-    def start
-      puts "Welcome to Connect Four"
-      puts "Player 1, please enter your name:"
-    end
   end
 end
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -4,10 +4,5 @@ module ConnectFour
   describe Game do
     let(:new_game) { ConnectFour::Game.new }
 
-    describe '#start' do
-      it 'displays a welcome message' do
-        expect { new_game.start }.to output(/Welcome to Connect Four/).to_stdout
-      end
-    end
   end
 end
