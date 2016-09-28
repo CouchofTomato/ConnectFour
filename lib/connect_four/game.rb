804633375991735aba6b145fd0a4b71be07d0271
Austin Mason <austinpmason@gmail.com>
2016-09-28 21:25:27 +0100

    new game specs

--- lib/connect_four/game.rb
+++ lib/connect_four/game.rb
@@ -1,8 +1,23 @@
 module ConnectFour
   class Game
+    attr_accessor :current_player, :board
+    attr_reader :players
 
     def initialize(player1, player2)
-      
+      @board = Board.new
+      @players = [player1, player2].cycle
+    end
+
+    def change_player
+      @current_player = @players.next
+    end
+
+    def is_draw?
+      @board.full?
+    end
+
+    def winner?
+      false
     end
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,13 +2,66 @@ require 'spec_helper'
 
 module ConnectFour
   describe Game do
-    subject(:game) { Game.new }
+    subject(:game) { Game.new(player1, player2) }
+    let(:player1) { ConnectFour::Player.new("Austin", :W) }
+    let(:player2) { ConnectFour::Player.new("Kevin", :B) }
+    let(:board) { game.board}
 
     it { expect(game).to be_instance_of Game }
 
     describe '#new' do
-      context 'creates a players enumberable object' do
-        it {expect(game.players).to be_kind_of Enumerable }
+      context 'creates a players variable' do
+        it {expect(game.players).to be_kind_of Enumerator }
+      end
+    end
+
+    describe '#change_player' do
+      context 'on the first turn' do
+        it 'player1 should be the current player' do
+          game.change_player
+          expect(game.current_player).to eql player1
+        end
+      end
+
+      context 'on the second turn' do
+        it 'player2 should be the current player' do
+          2.times { game.change_player }
+          expect(game.current_player).to eql player2
+        end
+      end
+    end
+
+    describe '#is_draw?' do
+      context 'when the board is not full' do
+        it 'should return false' do
+          expect(game.is_draw?).to be false
+        end
+      end
+
+      context 'when the board is full' do
+        it 'should return true' do
+          board.game_board.map!{|row| row.map {|col| col = :R}}
+          expect(game.is_draw?).to be true
+        end
+      end
+    end
+
+    describe '#winner?' do
+      context 'when there is no winner' do
+        it 'should return false' do
+          expect(game.winner?).to be false
+        end
+      end
+
+      context 'when there is a winner' do
+        context 'when there are 4 of the same piece in the same column' do
+          it 'should return true' do
+            4.times do
+              game.board.place_piece(2, :R)
+            end
+            expect(game.winner?).to be true
+          end
+        end
       end
     end
   end
